c3d5ac9af28274586ba394607c6f13182c97a545
Matias Munera <matiasmunera08@gmail.com>
2025-10-21 10:19:56 -0500

    Se editó escritura.py

--- archivos/escritura.py
+++ archivos/escritura.py
@@ -1,4 +1,6 @@
 ubicacion = "C:\\Users\\B09S202est\\Desktop\\archivos" # \ se usa para comandos de texto
 nombre_archivo = "frutas.txt"
 modo = "w" # Solo escritura - sobreescribe
-fp = open(ubicacion+"\\"+nombre_archivo, modo, ) # C:\Users\B09S202est\Desktop\archivos\frutas.txt
+fp = open(ubicacion+"\\"+nombre_archivo, modo, encoding="utf-8") # C:\Users\B09S202est\Desktop\archivos\frutas.txt
+
+fp.close()
