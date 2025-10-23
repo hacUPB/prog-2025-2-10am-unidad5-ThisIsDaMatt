80234762c41435ae2965fd9c458a0d37f46f222d
Matias Munera <matiasmunera08@gmail.com>
2025-10-23 10:54:41 -0500

    si

--- archivos/leer_csv.py
+++ archivos/leer_csv.py
@@ -2,6 +2,13 @@ import csv
 
 with open("C:\\Users\\B09S202est\\Desktop\\archivos" + "\\sensores.csv", "r") as csvfile:
     lector = csv.reader(csvfile, delimiter=";") # Se utiliza el metodo reader
-    print(lector)
+    encabezado = next(lector) # 
+    # print(encabezado)
+    presion = []
+    print(encabezado[0])
     for fila in lector: # Con el for se itera sobre el objeto para leer
-        print(fila)
+        fila[3] = fila[3].replace(",", ".")
+        dato = float(fila[3])
+        presion.append(dato)
+
+print(presion)
